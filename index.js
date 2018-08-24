b6499768fc052f96b0426a22b52b76204bf354cd
Oleksii Korshenko <okorshenko@magento.com>
2018-08-24 14:12:07 -0500

    Added basic functionality

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ module.exports = app => {
   app.log('Yay, the app was loaded!')
 
   app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+    const text = 'Hi @' + context.payload.issue.user.login + '. Thank you for your contribution \n' +
+      'Here is some useful tips how you can test your changes using Magento test environment.\n' +
+      'Add the comment under your pull request to deploy test or vanilla Magento instance:\n' +
+      '- `@magento-engcom-team give me test instance` - deploy test instance based on PR changes\n' +
+      '- `@magento-engcom-team give me {$VERSION} instance` - deploy vanilla Magento instance\n' +
+      '\n' +
+      'For more details, please, review the [Magento Contributor Assistant](https://devdocs.magento.com/guides/v2.2/contributor-guide/contributing.html#contributor-assist) documentation'
+    const issueComment = context.issue({body: text})
     return context.github.issues.createComment(issueComment)
   })
 
